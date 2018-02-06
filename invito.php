2620f7de09d4df681966c1ec3aebc66c3295b950
giuseppe sorce <giuseppe.sorce@warpmobile.it>
2018-02-06 08:49:41 +0100

    first commit

---
+++ invito.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="An awesome coming soon page designed by freshdesignweb" />
+<meta name="keywords" content="coming, soon, page, Comotia, free download" />
+
+<title>Android Devs Italy</title>
+
+<link rel="stylesheet" href="style.css">
+
+<style type="text/css">
+</style>
+
+</head>
+
+<body>
+
+<div id="coming-soon">
+
+<h1>Android Developers Italia</h1>
+
+<p>Grazie per esserti iscritto, riceverai presto un' email</p>
+
+
+
+
+
+</div>
+
+</body>
+</html>
